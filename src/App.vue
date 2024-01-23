5905f6d6a66b8a6e03f75602e711b1986ed7d845
IKIGAI LABS XYZ <128307722+IkigaiLabsETH@users.noreply.github.com>
2024-01-23 21:52:19 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import code from '@code-wallet/elements';
+import { onMounted, ref } from 'vue'
+
+const el = ref(null);
+
+onMounted(() => {
+  const { button } = code.elements.create('button', {
+    currency: 'usd',
+    destination: 'E8otxw1CVX9bfyddKu3ZB3BVLa4VVF9J7CTPdnUwT9jR',
+    amount: 0.05,
+  });
+
+  button.mount(el.value);
+})
+</script>
+
+<template>
+  <div ref="el"></div>
+</template>
